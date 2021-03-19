03a1811d9888fd49e6cf6b09833ef1082fc87a8a
tissarni <tissarni>
2021-03-19 08:21:10 +0100

    home can ask  and server can give energies

--- market.py
+++ market.py
@@ -1,45 +1,50 @@
 
-import sys 
+import sys
 import time
 import sysv_ipc
 import threading
 
-key = 667
-# ipcrm -Q 666 
+key = 706
+# ipcrm -Q 666
 # pour kill la message queue
+energie = 12
+
 
 def worker(mq, m):
     print("Starting thread:", threading.current_thread().name)
-    dt = time.asctime()      
-    message = str(dt).encode()
     pid = int(m.decode())
     t = pid + 3
+    if pid > energie:
+        message = energie
+    if pid <= energie:
+        message = m
+    message = str(message).encode()
+    #pid = int(m.decode())
+    #t = pid + 3
     mq.send(message, type=t)
-    print("Ending thread:", threading.current_thread().name)    
+    print(t)
+    print("Ending thread:", threading.current_thread().name)
+
 
 if __name__ == "__main__":
     print("Starting thread:", threading.current_thread().name)
-    
+
     try:
         mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREX)
     except:
         print("Message queue", key, "already exsits, terminating.")
         sys.exit(1)
 
-    print("Starting time server.")
+    print("Starting energy market.")
 
     threads = []
     while True:
         m, t = mq.receive()
-        if t == 1:
-            p = threading.Thread(target=worker, args=(mq, m))
-            p.start()
-            threads.append(p)
-        if t == 2:
-            for thread in threads:
-                thread.join()
-            mq.remove()                
-            break
-    
-    print("Terminating time server.")
-    print("Ending thread:", threading.current_thread().name)    
+
+        p = threading.Thread(target=worker, args=(mq, m))
+        p.start()
+        threads.append(p)
+        break
+
+    print("Terminating energy market.")
+    print("Ending thread:", threading.current_thread().name)
