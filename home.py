dfbc0f3999f12df2f05959dc5af6d099eccf22a1
ncavalierc <nicolas.cavalier-coulet@insa-lyon>
2021-03-26 18:28:30 +0100

    a faire : barrier dans le home, corriger demande, processus externe

--- home.py
+++ home.py
@@ -4,49 +4,52 @@ import sys
 import time
 import sysv_ipc
 from multiprocessing import Process, Manager
+import multiprocessing
+import random
 
 key = 666
 
 def user(lst):
     print("PID fils home: " + str(os.getpid()))
-    '''
-    choix = 0
-    answer = 0
-    print("Que voulez-vous faire ?")
-    print("1. Vendre")
-    print("2. Acheter")
-    choix = 2
-    if choix == 1:
-        print("toto")
-    elif choix == 2:
-        while answer not in range(1, 100):
-            print("Combien d'énergie voulez vous")
-            answer = 5
-        return answer
-    '''
-    energie = 0
-    wallet = 100
-    
-    demande = 5
 
-    m = demande
-    w = wallet
-    data = str(m)+ "," + str(w)
-    print("1", data)
-    mq.send(str(data).encode(), type=1)
-    print("2")
-    demande += 3
-    m, demande = mq.receive(type=demande)
-    print("3")
-    recu = m.decode()
-    recu = recu.split(",")
-    print("4", recu)
-    quantite_recue = int(recu[0])
-    prix = int(recu[1])
-    wallet -= quantite_recue * prix
-    energie += quantite_recue
-    print("Energie reçue : ", quantite_recue, "Prix unitaire : ", prix)
-    time.sleep(1)
+    energie = 0
+    wallet = 100000
+    
+    while True:
+        demande = int(10*random.random()) * int(lst.value)
+        m = demande
+        w = wallet
+        data = str(m)+ "," + str(w)
+        print("1", data)
+        mq.send(str(data).encode(), type=1)
+        print("2")
+        demande += 3
+        m, demande = mq.receive(type=demande)
+        print("3")
+        recu = m.decode()
+        recu = recu.split(",")
+        print("4", recu)
+        quantite_recue = int(recu[0])
+        prix = int(recu[1])
+        wallet -= quantite_recue * prix
+        energie += quantite_recue
+        print("Energie reçue : ", quantite_recue, "Prix unitaire : ", prix)
+        time.sleep(1)
+
+def temperature(lst, lock):
+    temperature = random.gauss(14,6)
+    coef = 0
+    if temperature < 10:
+        coef = 2
+    elif temperature < 20:
+        coef = 1
+    elif temperature >= 20:
+        coef = 0.5
+
+    print("coeff", coef)
+
+    with lst.get_lock():
+                lst.value = int(coef)
 
 
 try:
@@ -57,14 +60,19 @@ except:
 
 
 if __name__ == "__main__":
-    while True:
-        with Manager() as manager:
-            lst = manager.list()
+    with Manager() as manager:
+        
+        lst = multiprocessing.Value('i', 0)
+        lock = multiprocessing.Lock()
 
+        weather = Process(target=temperature, args=(lst, lock))
+        weather.start()
+
+        for i in range(5):
             child = Process(target=user, args=(lst,))
             child.start()
 
-            print("PID parent home: " + str(os.getpid())) 
-
-            child.join()
+        print("PID parent home: " + str(os.getpid())) 
+            
         print("fini")
+        child.join()
--- market.py
+++ market.py
@@ -5,14 +5,24 @@ import sysv_ipc
 import threading
 import concurrent.futures
 
-key = 666
 # ipcrm -Q 666
 # pour kill la message queue
+key = 666
 energie = 10000
-prix = 2
+prix = 50
+prix_1 = 50
+jour = 0
+maBarrier = threading.Barrier(5)
+jour_0 = 10
+jour_J = 0
 
 def worker(mq, m):
     global energie
+    global jour
+    global jour_0
+    global jour_J
+    global prix
+    global prix_1
     print("Starting thread:", threading.current_thread().name)
     data = m.decode()
     data = data.split(",")
@@ -26,16 +36,39 @@ def worker(mq, m):
     else:
         message = demande / prix
     
-    
     message = str(message)
     prix_encoded = str(prix)
     data = str(message) + "," + str(prix_encoded)
     mq.send(data.encode(), type=t)
+
+    jour_J += demande
+
+    maBarrier.wait()
+    
     print("Ending thread:", threading.current_thread().name)
 
+    if maBarrier.wait() == 0:
+        if jour_J > jour_0:
+            rapport = (jour_J / jour_0) / 10
+            tmp = prix
+            prix = int(prix_1 + prix_1 * rapport)
+            prix_1 = tmp
+        elif jour_J < jour_0:
+            rapport = (jour_0 / jour_J) / 10
+            tmp = prix
+            prix = int(prix_1 - prix_1 * rapport)
+            prix_1 = tmp
+        
+        print("Prix : ", prix)
 
+        jour_0 = jour_J
+        jour_J = 0
+        jour += 1
+        print("Jour : ", jour)
+    
+    
 if __name__ == "__main__":
-    print("Starting thread:", threading.current_thread().name)
+    print("Starting thread parent:", threading.current_thread().name)
 
     try:
         mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREX)
@@ -45,10 +78,10 @@ if __name__ == "__main__":
 
     print("Starting energy market.")
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers = 4) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers = 5) as executor:
         while True:
             m, t = mq.receive(type=1)
             executor.submit(worker, mq, m)
 
     print("Terminating energy market.")
-    print("Ending thread:", threading.current_thread().name)
+    print("Ending thread parent :", threading.current_thread().name)
